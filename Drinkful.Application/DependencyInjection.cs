58435431a7c74a839e039d69ac9c10165e01028a
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-10-29 15:42:42 +0300

    Add dependency injection for application layer

---
+++ Drinkful.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Drinkful.Application.Services.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drinkful.Application; 
+
+public static class DependencyInjection {
+  public static IServiceCollection AddApplication(this IServiceCollection services) {
+    services.AddScoped<IAuthenticationService, AuthenticationService>();
+    return services;
+  }
+}
