38577e25898692092c6d439c5996679981e511b1
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-10-30 11:04:29 +0200

    Add dependency injection for infrastructure layer

--- Drinkful.API/Program.cs
+++ Drinkful.API/Program.cs
@@ -1,9 +1,11 @@
 using Drinkful.Application;
+using Drinkful.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,10 +14,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ Drinkful.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Drinkful.Application.Common.Interfaces.Persistence;
+using Drinkful.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drinkful.Infrastructure;
+
+public static class DependencyInjection {
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+    services.AddScoped<IUserRepository, UserRepository>();
+    return services;
+  }
+}
