cc054858adc320820a2d8c1caff95365d5134d0d
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-10-30 11:15:10 +0200

    Basis for an authentication service

--- Drinkful.Application/Services/Authentication/AuthenticationService.cs
+++ Drinkful.Application/Services/Authentication/AuthenticationService.cs
@@ -1,11 +1,38 @@
-﻿namespace Drinkful.Application.Services.Authentication;
+﻿using Drinkful.Application.Common.Interfaces.Persistence;
+using Drinkful.Domain.Entities;
+
+namespace Drinkful.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService {
+  private readonly IUserRepository _userRepository;
+
+  public AuthenticationService(IUserRepository userRepository) {
+    _userRepository = userRepository;
+  }
+
   public AuthenticationResult Login(string email, string password) {
-    throw new NotImplementedException();
+    var user = _userRepository.GetByEmail(email);
+    if (user is null) {
+      throw new Exception("User with the given email does not exist.");
+    }
+
+    // TODO Proper password hashing comparison
+    if (user.PasswordHash != password) {
+      throw new Exception("Invalid password.");
+    }
+
+    // TODO JWT generation
+    return new AuthenticationResult(user, "token");
   }
 
   public AuthenticationResult Register(string username, string email, string password) {
-    throw new NotImplementedException();
+    if (_userRepository.GetByEmail(email) is not null) {
+      throw new Exception("User with the given email already exists.");
+    }
+
+    // TODO Proper password hashing
+    var user = new User { Username = username, Email = email, PasswordHash = password };
+    // TODO JWT generation
+    return new AuthenticationResult(user, "token");
   }
 }
