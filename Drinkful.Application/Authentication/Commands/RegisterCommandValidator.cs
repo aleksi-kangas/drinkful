01ea4d2d46d53feaacfc48ca64b0b66080f52048
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-11-02 20:05:44 +0200

    Preliminary validators for RegisterCommand and LoginQuery

---
+++ Drinkful.Application/Authentication/Commands/RegisterCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Drinkful.Application.Authentication.Commands; 
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand> {
+  public RegisterCommandValidator() {
+    RuleFor(x => x.Username).NotEmpty();
+    RuleFor(x => x.Email).NotEmpty();
+    RuleFor(x => x.Password).NotEmpty();
+  }
+}
---
+++ Drinkful.Application/Authentication/Queries/LoginQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Drinkful.Application.Authentication.Queries;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery> {
+  public LoginQueryValidator() {
+    RuleFor(x => x.Email).NotEmpty();
+    RuleFor(x => x.Password).NotEmpty();
+  }
+}
