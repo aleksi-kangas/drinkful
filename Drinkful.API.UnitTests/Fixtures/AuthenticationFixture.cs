174f99955c327d63c466402f5b17ab1cd061bba3
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-11-02 13:29:18 +0200

    Refactor unit tests for AuthenticationController

---
+++ Drinkful.API.UnitTests/Fixtures/AuthenticationFixture.cs
@@ -0,0 +1,34 @@
+﻿using Drinkful.Application.Authentication.Common;
+using Drinkful.Contracts.Authentication;
+using Drinkful.Domain.Entities;
+using ErrorOr;
+
+namespace Drinkful.API.UnitTests.Fixtures;
+
+public static class AuthenticationFixture {
+  public class TestUser : User {
+    public string Password { get; set; } = null!;
+  }
+
+  public static ErrorOr<AuthenticationResult> GetAuthenticationResultSuccess(TestUser user) {
+    return new AuthenticationResult(user, "token");
+  }
+  
+  public static ErrorOr<AuthenticationResult> GetAuthenticationResultFailure(Error error) {
+    return error;
+  }
+  
+  public static AuthenticationResponse GetAuthenticationResponseSuccess(TestUser user) {
+    return new AuthenticationResponse(user.Id, user.Username, user.Email, "token");
+  }
+
+  public static TestUser GetTestUser() {
+    return new TestUser {
+      Id = Guid.NewGuid(),
+      Email = "user@example.com",
+      Password = "password",
+      PasswordHash = "passwordHash",
+      Username = "username"
+    };
+  }
+}
--- Drinkful.API.UnitTests/Systems/Controllers/TestAuthenticationController.cs
+++ Drinkful.API.UnitTests/Systems/Controllers/TestAuthenticationController.cs
@@ -1,10 +1,10 @@
 ﻿using Drinkful.API.Controllers;
+using Drinkful.API.UnitTests.Fixtures;
 using Drinkful.Application.Authentication.Commands;
 using Drinkful.Application.Authentication.Common;
 using Drinkful.Application.Authentication.Queries;
 using Drinkful.Contracts.Authentication;
-using Drinkful.Domain.Entities;
-using ErrorOr;
+using Drinkful.Domain.Common.Errors;
 using FluentAssertions;
 using MapsterMapper;
 using MediatR;
@@ -14,26 +14,28 @@ using Moq;
 namespace Drinkful.API.UnitTests.Systems.Controllers;
 
 public class TestAuthenticationController {
+  private readonly Mock<ISender> _mediatorMock;
+  private readonly Mock<IMapper> _mapperMock;
+
+  public TestAuthenticationController() {
+    _mediatorMock = new Mock<ISender>();
+    _mapperMock = new Mock<IMapper>();
+  }
+
   [Fact]
   public async void Login_OnSuccessReturns200() {
     // Arrange
-    var loginQuery = new LoginQuery("user@example.com", "password");
-    ErrorOr<AuthenticationResult> authResult = new AuthenticationResult(new User(), "token");
-    var mockSender = new Mock<ISender>();
-    mockSender.Setup(x => x.Send(loginQuery, It.IsAny<CancellationToken>()))
-      .Returns(Task.FromResult(authResult));
-
-    var mockMapper = new Mock<IMapper>();
-    mockMapper.Setup(x => x
-      .Map<LoginRequest, LoginQuery>(It.IsAny<LoginRequest>()))
+    var testUser = AuthenticationFixture.GetTestUser();
+    var loginQuery = new LoginQuery(testUser.Email, testUser.Password);
+    _mapperMock.Setup(x => x.Map<LoginQuery>(It.IsAny<LoginRequest>()))
       .Returns(loginQuery);
-    mockMapper.Setup(x => x
-      .Map<AuthenticationResult, AuthenticationResponse>(It.IsAny<AuthenticationResult>()))
-      .Returns(new AuthenticationResponse(Guid.NewGuid(), "", "", ""));
-
-    var controller = new AuthenticationController(mockSender.Object, mockMapper.Object);
+    _mediatorMock.Setup(x => x.Send(loginQuery, It.IsAny<CancellationToken>()))
+      .ReturnsAsync(AuthenticationFixture.GetAuthenticationResultSuccess(testUser));
+    _mapperMock.Setup(x => x.Map<AuthenticationResponse>(It.IsAny<AuthenticationResult>()))
+      .Returns(AuthenticationFixture.GetAuthenticationResponseSuccess(testUser));
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
     // Act
-    var request = new LoginRequest("user@example.com", "password");
+    var request = new LoginRequest(testUser.Email, testUser.Password);
     var result = await controller.Login(request);
     // Assert
     result.Should().BeOfType<OkObjectResult>();
@@ -42,26 +44,78 @@ public class TestAuthenticationController {
   }
 
   [Fact]
+  public async void Login_InvokesMediator() {
+    // Arrange
+    var testUser = AuthenticationFixture.GetTestUser();
+    _mapperMock.Setup(x => x.Map<LoginQuery>(It.IsAny<LoginRequest>()))
+      .Returns(new LoginQuery(testUser.Email, testUser.Password));
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
+    // Act
+    await controller.Login(new LoginRequest(testUser.Email, testUser.Password));
+    // Assert
+    _mediatorMock.Verify(x => x.Send(It.IsAny<LoginQuery>(), It.IsAny<CancellationToken>()),
+      Times.Once);
+  }
+
+  [Fact]
+  public async void Login_InvokesMapperForRequest() {
+    // Arrange
+    var testUser = AuthenticationFixture.GetTestUser();
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
+    // Act
+    await controller.Login(new LoginRequest(testUser.Email, testUser.Password));
+    // Assert
+    _mapperMock.Verify(x => x.Map<LoginQuery>(It.IsAny<LoginRequest>()),
+      Times.Once);
+  }
+
+  [Fact]
+  public async void Login_InvokesMapperForResponse() {
+    // Arrange
+    var testUser = AuthenticationFixture.GetTestUser();
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
+    // Act
+    await controller.Login(new LoginRequest(testUser.Email, testUser.Password));
+    // Assert
+    _mapperMock.Verify(x => x.Map<AuthenticationResponse>(It.IsAny<AuthenticationResult>()),
+      Times.Once);
+  }
+
+  [Fact]
+  public async void Login_OnInvalidCredentialsReturns400() {
+    // Arrange
+    var testUser = AuthenticationFixture.GetTestUser();
+    var loginQuery = new LoginQuery("user@example.com", "password");
+    _mapperMock.Setup(x => x.Map<LoginQuery>(It.IsAny<LoginRequest>()))
+      .Returns(loginQuery);
+    _mediatorMock.Setup(x => x.Send(loginQuery, It.IsAny<CancellationToken>()))
+      .ReturnsAsync(
+        AuthenticationFixture.GetAuthenticationResultFailure(Errors.Authentication
+          .InvalidCredentials));
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
+    // Act
+    var request = new LoginRequest(testUser.Email, testUser.Password);
+    var result = await controller.Login(request);
+    // Assert
+    result.Should().BeOfType<ObjectResult>();
+    var objectResult = (ObjectResult)result;
+    objectResult.StatusCode.Should().Be(400);
+  }
+
+  [Fact]
   public async void Register_OnSuccessReturns200() {
     // Arrange
-    var registerCommand = new RegisterCommand("username", "user@example.com", "password");
-    ErrorOr<AuthenticationResult> authResult = new AuthenticationResult(new User(), "token");
-    var mockSender = new Mock<ISender>();
-    mockSender.Setup(x => x.Send(registerCommand, It.IsAny<CancellationToken>()))
-      .Returns(Task.FromResult(authResult));
-    
-    var mockMapper = new Mock<IMapper>();
-    mockMapper.Setup(x => x
-        .Map<RegisterRequest, RegisterCommand>(It.IsAny<RegisterRequest>()))
+    var testUser = AuthenticationFixture.GetTestUser();
+    var registerCommand = new RegisterCommand(testUser.Username, testUser.Email, testUser.Password);
+    _mapperMock.Setup(x => x.Map<RegisterCommand>(It.IsAny<RegisterRequest>()))
       .Returns(registerCommand);
-    mockMapper.Setup(x => x
-        .Map<AuthenticationResult, AuthenticationResponse>(It.IsAny<AuthenticationResult>()))
-      .Returns(new AuthenticationResponse(Guid.NewGuid(), "", "", ""));
-
-    var controller = new AuthenticationController(mockSender.Object, mockMapper.Object);
+    _mediatorMock.Setup(x => x.Send(registerCommand, It.IsAny<CancellationToken>()))
+      .ReturnsAsync(AuthenticationFixture.GetAuthenticationResultSuccess(testUser));
+    _mapperMock.Setup(x => x.Map<AuthenticationResponse>(It.IsAny<AuthenticationResult>()))
+      .Returns(AuthenticationFixture.GetAuthenticationResponseSuccess(testUser));
+    var controller = new AuthenticationController(_mediatorMock.Object, _mapperMock.Object);
     // Act
-    var request = new RegisterRequest("username", "user@example.com", "password");
-    var result = await controller.Register(request);
+    var result = await controller.Register(new RegisterRequest(testUser.Username, testUser.Email, testUser.Password));
     // Assert
     result.Should().BeOfType<OkObjectResult>();
     var okResult = (OkObjectResult)result;
