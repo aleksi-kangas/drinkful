ea0565bb5cb8231c10fb002fd49532b8a9af9cfa
Aleksi Kangas <aleksi.kangas@hotmail.com>
2022-11-05 15:30:55 +0200

    Add unit tests for RegisterCommand and LoginQuery validators

---
+++ Drinkful.Application.UnitTests/Systems/Authentication/Commands/TestRegisterCommandValidator.cs
@@ -0,0 +1,74 @@
+﻿using Drinkful.Application.Authentication.Commands;
+using FluentValidation.TestHelper;
+
+namespace Drinkful.Application.UnitTests.Systems.Authentication.Commands;
+
+public class TestRegisterCommandValidator {
+  private readonly RegisterCommandValidator _validator = new();
+  
+  [Theory]
+  [InlineData("", "user@example.com", "password")]
+  [InlineData("", "", "")]
+  public void RegisterCommand_ShouldHaveError_WhenUsernameIsEmpty(
+    string username,
+    string email,
+    string password) {
+    // Arrange
+    var registerCommand = new RegisterCommand(username, email, password);
+    // Act
+    var result = _validator.TestValidate(registerCommand);
+    // Assert
+    result
+      .ShouldHaveValidationErrorFor(rc => rc.Username)
+      .WithErrorCode("NotEmptyValidator");
+  }
+  
+  [Theory]
+  [InlineData("username", "", "password")]
+  [InlineData("", "", "")]
+  public void RegisterCommand_ShouldHaveError_WhenEmailIsEmpty(
+    string username,
+    string email,
+    string password) {
+    // Arrange
+    var registerCommand = new RegisterCommand(username, email, password);
+    // Act
+    var result = _validator.TestValidate(registerCommand);
+    // Assert
+    result
+      .ShouldHaveValidationErrorFor(rc => rc.Email)
+      .WithErrorCode("NotEmptyValidator");
+  }
+  
+  [Theory]
+  [InlineData("username", "user@example.com", "")]
+  [InlineData("", "", "")]
+  public void RegisterCommand_ShouldHaveError_WhenPasswordIsEmpty(
+    string username,
+    string email,
+    string password) {
+    // Arrange
+    var registerCommand = new RegisterCommand(username, email, password);
+    // Act
+    var result = _validator.TestValidate(registerCommand);
+    // Assert
+    result
+      .ShouldHaveValidationErrorFor(rc => rc.Password)
+      .WithErrorCode("NotEmptyValidator");
+  }
+  
+  [Theory]
+  [InlineData("username", "user@example.com", "password")]
+  [InlineData("somebody", "somebody@gmail.com", "123456")]
+  public void RegisterCommand_ShouldNotHaveAnyErrors_WhenUsernameEmailAndPasswordAreNonEmpty(
+    string username,
+    string email,
+    string password) {
+    // Arrange
+    var loginQuery = new RegisterCommand(username, email, password);
+    // Act
+    var result = _validator.TestValidate(loginQuery);
+    // Assert
+    result.ShouldNotHaveAnyValidationErrors();
+  }
+}
---
+++ Drinkful.Application.UnitTests/Systems/Authentication/Queries/TestLoginQueryValidator.cs
@@ -0,0 +1,50 @@
+﻿using Drinkful.Application.Authentication.Queries;
+using FluentValidation.TestHelper;
+
+namespace Drinkful.Application.UnitTests.Systems.Authentication.Queries;
+
+public class TestLoginQueryValidator {
+  private readonly LoginQueryValidator _validator = new();
+
+  [Theory]
+  [InlineData("", "password")]
+  [InlineData("", "")]
+  public void LoginQuery_ShouldHaveError_WhenEmailIsEmpty(string email, string password) {
+    // Arrange
+    var loginQuery = new LoginQuery(email, password);
+    // Act
+    var result = _validator.TestValidate(loginQuery);
+    // Assert
+    result
+      .ShouldHaveValidationErrorFor(lq => lq.Email)
+      .WithErrorCode("NotEmptyValidator");
+  }
+
+  [Theory]
+  [InlineData("user@example.com", "")]
+  [InlineData("", "")]
+  public void LoginQuery_ShouldHaveError_WhenPasswordIsEmpty(string email, string password) {
+    // Arrange
+    var loginQuery = new LoginQuery(email, password);
+    // Act
+    var result = _validator.TestValidate(loginQuery);
+    // Assert
+    result
+      .ShouldHaveValidationErrorFor(lq => lq.Password)
+      .WithErrorCode("NotEmptyValidator");
+  }
+
+  [Theory]
+  [InlineData("user@example.com", "password")]
+  [InlineData("somebody@gmail.com", "123456")]
+  public void LoginQuery_ShouldNotHaveAnyErrors_WhenEmailAndPasswordAreNonEmpty(
+    string email,
+    string password) {
+    // Arrange
+    var loginQuery = new LoginQuery(email, password);
+    // Act
+    var result = _validator.TestValidate(loginQuery);
+    // Assert
+    result.ShouldNotHaveAnyValidationErrors();
+  }
+}
