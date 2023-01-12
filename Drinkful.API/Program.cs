ced99adb07bf56380aeda0f8289b96a2a704c6c8
Aleksi Kangas <aleksi.kangas@hotmail.com>
2023-01-12 12:53:41 +0200

    Add Bearer JWT support to Swagger

--- Drinkful.API/Program.cs
+++ Drinkful.API/Program.cs
@@ -1,6 +1,7 @@
 using Drinkful.API;
 using Drinkful.Application;
 using Drinkful.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,26 @@ builder.Services
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => {
+  o.SwaggerDoc("v1", new OpenApiInfo { Title = "JWTToken_Auth_API", Version = "v1" });
+  o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() {
+    Name = "Authorization",
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description =
+      "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+  });
+  o.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+      new OpenApiSecurityScheme {
+        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+      },
+      Array.Empty<string>()
+    }
+  });
+});
 
 var app = builder.Build();
 
